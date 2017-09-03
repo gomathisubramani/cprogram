838e3c3bfeea6c5363f0f6a6e6cc305f2a188f67
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2017-09-03 12:31:57 +0530

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+scanf("%d",&n);
+if(n>=0)
+{
+printf("the number is positive");
+}
+if(n<=0)
+{
+pritf("the number is negative");
+}
+if(n==0)
+{
+printf("the number is same");
+}
+}
