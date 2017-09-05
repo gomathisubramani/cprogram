b50e4ee9eacf8ff0d034034626c24ab804c2164b
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2017-09-05 06:24:38 -0700

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+scanf("%d",&n);
+if(n>=0)
+{
+printf("the number is positive");
+}
+else if(n<=0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is zero");
+}
+}
