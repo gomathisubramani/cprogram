2ef317e29fd854da94939cdfdf85bf8494730942
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2017-09-25 06:31:54 -0700

    Create power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int base,exponent;
+long long result=1;
+printf("enter the base");
+scanf("%d",&base);
+printf("enter the exponent");
+scanf("%d",&exponent);
+while(exponent!=0)
+{
+result*=base;
+--exponent;
+}
+printf("enter the result");
+}
