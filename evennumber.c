162a06c4d39a78fd28218a8ee8b56ad083efb90d
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-01-10 17:00:34 +0530

    Create evennumber.c

---
+++ evennumber.c
@@ -0,0 +1,26 @@
+int main()
+{
+	int a,b,i,rem;
+	printf("enter the first range of a");
+	scanf("%d",&a);
+	printf("enter the second range of b");
+	scanf("%d",&b);
+	for(i=a;i<=b;++i)
+	
+		rem=i/2;
+		if(rem==0)
+		#include<stdio.h>
+int main()
+{
+	int a,b,i,rem;
+	printf("enter the first range of a");
+	scanf("%d",&a);
+	printf("enter the second range of b");
+	scanf("%d",&b);
+	for(i=a;i<=b;++i)
+	
+		rem=i/2;
+		if(rem==0)
+		
+	
+	printf("%d",i);
