a9927758b18b68e107793788048509e434a6ce95
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-01-19 17:01:06 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[60],i,size,k;
+    printf("enter the size of the array");
+    scanf("%d",&size);
+    printf("enter the elements of the array");
+    scanf("%d",&a[i]);
+    k=a[0];
+    for(i=1;i<Lsize;i++)
+    {
+        if(k<=a[i])
+        {
+            printf("%d",k);
+        }
+    }
+}
