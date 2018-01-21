dbedf0ded28c4fc86fc42c87327bb0c2445a385b
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-01-21 05:54:55 +0000

    Create poweroftwo.c

---
+++ poweroftwo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+      if((a%2)==0)
+    
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+}
