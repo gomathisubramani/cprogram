57fad725fdd4edc867d4aa9bf5c11bc8bf66bad4
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-01-28 15:04:50 +0530

    Create 24sort.c

---
+++ 24sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void ascending();
+void main()
+{
+	ascending();
+}
+void ascending()
+{
+	int i,j,a[20],t,n;
+	printf("enter the element");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("the sorted element are");
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+	
+			}
+	
