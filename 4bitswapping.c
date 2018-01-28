1bfca5ec3535daffa54684b618a43897843d2e59
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-01-28 16:18:33 +0530

    Create 4bitswapping.c

---
+++ 4bitswapping.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int *,int *);
+int main()
+{
+    int a,b;
+    printf("enter the number");
+    scanf("%d%d",&a,&b);
+    printf("number is before swapping %d%d",a,b);
+    swap(&a,&b);
+    printf("number after swapping %d%d",a,b);
+}
+void swap(int *a,int *b)
+{
+    *a=*a+*b;
+    *b=*a-*b;
+    *a=*a-*b;
+    printf("swap function a&b values are %d%d",*a,*b);
+}
