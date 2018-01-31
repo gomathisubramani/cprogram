7ef1f10e657353ec5a0625c5a22391da6e27000e
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-01-31 15:50:40 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    char b[100]="welcome 23456";
+    int i,count=0;
+    for(i=0;b[i]!='\0';i++)
+    {
+        if(b[i]=='0'||b[i]=='1'||b[i]=='2'||b[i]=='3'||b[i]=='4'||b[i]=='5'||b[i]=='6'||b[i]=='7'||b[i]=='8'||b[i]=='9')
+        count++;
+        
+    }
+    printf("the number of neumeric character %d",count);
+}
