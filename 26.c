c9f25bf7a6fe4b555cfa666eb69b7799a0957454
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-05 15:55:51 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,a[20],t,n,k,h;
+	printf("enter the element");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
