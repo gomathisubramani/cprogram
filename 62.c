67796de56007689151e66b78071f1241e8a6be2d
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-05 16:13:26 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char b[30];
+    int i;
+    printf("enter the string");
+    scanf("%s",b);
+    if(b[i]=='1'||b[i]=='0')
+    {
+        printf("the given number is binary");
+    }
+    else
+    {
+        printf("the number is not binary");
+    }
+    return 0;
+}
