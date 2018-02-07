e61866025e93a42358399f154facaa6745a2b7fe
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-07 17:00:23 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the number");
+    scanf("%d",c);
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",b);
+    if((c>a)&&(c<b))
+    {
+    printf("yes");
+    }
+    else
+    {
+        printf("no");
+        
+    }
+}
