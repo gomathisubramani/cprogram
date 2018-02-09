e604e655cc7a094455d771f5f5e5a8da141db41d
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-09 16:06:27 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float a;
+    int b;
+    printf("enter the number");
+    scanf("%f",&a);
+    b=round(a);
+    printf("the nearest integer is %d",b);
+    
+}
