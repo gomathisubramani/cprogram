a3d2ca8e0b8414f0da500f305b176596df7c807c
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-19 16:03:55 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	float a,b,c;
+	printf("enter the length of the farm");
+	scanf("%f",&a);
+	printf("enter the breadth of the farm");
+	scanf("%f",&b);
+	c=a*b;
+	printf("the area of the rectangular farm is %.5f",c);
+}
