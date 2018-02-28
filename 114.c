4895e9519b9a84c7740f92c2c90b14f23607ef99
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-28 06:14:39 -0800

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int n,k,count=0,i,a[50];
+    printf("enter the number");
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if((a[i]%2)!=0)
+        {
+            count++;
+        
+        if(count==k)
+        {
+         printf("%d",a[i]);   
+        }
+        }
+        
+    }
+
+}
