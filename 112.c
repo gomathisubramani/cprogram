1d21b1818b65ec8b2f732d92344e7d8b3728a3e2
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-28 05:46:34 -0800

    Create 112.c

---
+++ 112.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int n,k,count=0,i,a[50];
+    printf("enter the number");
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            count++;
+        }
+    }
+    if(count==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+}
