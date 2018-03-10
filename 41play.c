70ab3f1f4c5352df05563d74e2c79e0ff418de60
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-03-10 14:26:02 +0530

    Create 41play.c

---
+++ 41play.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+	int n,k;
+	printf("\n enter the number");
+	scanf("%d",&n);
+	printf("\n enter the value");
+	scanf("%d",&k);
+	if(n==k*k)
+	{
+		printf("\n YES");
+	}
+	else
+	{
+		printf("\n NO");
+	}
+}
