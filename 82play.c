1eeac346e92495c4da277f5a94e814db41703174
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-03-12 16:15:14 +0530

    Create 82play.c

---
+++ 82play.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,h;
+	char a[10];
+	printf("\n enter the number");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	h=a[0];
+	for(i=0;i<n;i++)
+	{
+		h=h&a[i];
+	}
+	printf("\n %d",h);
+}
