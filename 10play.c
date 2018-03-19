0ef37020b02fe004b6d9ef637df716f345c43ee4
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-03-19 10:47:32 +0000

    Create 10play.c

---
+++ 10play.c
@@ -0,0 +1,1 @@
+#include <stdio.h>#include<string.h>void main(){ char a[10],b[10]; int l,k,cont=0; printf("\n enter the string"); scanf("%s",a); printf("\n enter the second string"); scanf("%s",b); l=strlen(a); k=strlen(b); for(i=0;i<l;i++) {  if(a[i]!=b[i])  {   count=count++;  } } if(count==1) {  printf("YES"); } else {  printf("NO"); }
