6a81438e88105d33abf176c6cd7eeca9488b82cd
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-03-24 06:50:53 -0700

    Create 138play.c

---
+++ 138play.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("enter the number");
+    scanf("%d",&a);
+    b==2^a;
+    if((b%2)==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
