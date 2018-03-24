245ec3c6fdbe3d133bdf7788d26a254110bf1871
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-03-24 07:21:29 -0700

    Create 140play.c

---
+++ 140play.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[70];
+    int i;
+    printf("enter the string");
+    scanf("%s",a);
+    if((a[i]=='a')&&(a[i]=='b'))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
